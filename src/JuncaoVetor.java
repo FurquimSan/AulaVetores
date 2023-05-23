abc31a3428d0f13b7e537bacd52bb31a32907093
Luiz <luiztfsanches@gmail.com>
2023-05-22 21:03:53 -0300

    metodos

--- src/JuncaoVetor.java
+++ src/JuncaoVetor.java
@@ -3,6 +3,7 @@ public class JuncaoVetor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+int vetor[] = new int[10];
 
 	}
 
--- src/TesteVetores.java
+++ src/TesteVetores.java
@@ -6,7 +6,7 @@ public static final int quantNum = 10;
 		
 		
 		int vetor [] = new int [quantNum];
-		int i;
+		int i = 0;
 		Scanner LeitorNum = new Scanner(System.in);
 		for(i=0; i < quantNum; i++ ) {
 			System.out.println("Digite o valor do" + (i+1) + " numero");
@@ -19,5 +19,40 @@ public static final int quantNum = 10;
 			}
 		}
 		System.out.println("O maior valor do vetor e: " + maior);
+		
+		//Somar os valores do vetor
+		int soma = 0;
+		double media = 0;
+		for (i=0; i< vetor.length; i++) {
+			soma += vetor[i];
+			System.out.println("A soma do vetor e:" + soma);
+		}
+		//Media dos elementos
+		media = (double) soma / vetor.length;
+				System.out.println("A media dos valores e:" + media);
+	
+	
+    //Exibir o indice do primeiro positivo inserido.
+	for (i=0; i< vetor.length; i++ ) {
+		if (vetor[i]>= 0) {
+			System.out.println("Indice do primeiro positivo e :" +i);
+			break;
+			
+		}
+	}
+	// Exibir o indice do primeito negativo
+	int negativo  = 0;
+	for(i=0; i<vetor.length; i++) {
+		if (vetor[i] < 0) {
+			negativo = i;
+		}
+	}
+	System.out.println("O ultimo indice negativo e" + negativo);
+	if (negativo == -1) {
+		System.out.println("Não possui indices negativos");
+	}
 	}
 }
+
+
+// Length = Correr por todo o vetor.
